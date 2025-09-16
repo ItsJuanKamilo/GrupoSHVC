b111ab04384320ad2ed2cdd9c2680b9475207a36
ItsJuanKamilo <ju.leiton@duocuc.cl>
2025-09-16 19:28:15 -0300

    fix: remove static export, enable SSR

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // Deshabilitar funcionalidades que no funcionan en S3
   skipTrailingSlashRedirect: true,
   skipMiddlewareUrlNormalize: true,
 }
